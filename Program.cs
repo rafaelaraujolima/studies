66ed844e7b7d85e1157718767954059aa714869b
Rafael Araujo de Lima <rafael.araujo.lima@outlook.com>
2024-02-24 17:13:17 -0400

    Solution To Contaisn Duplicate

--- Program.cs
+++ Program.cs
@@ -6,6 +6,6 @@ public static class Program
 {
     public static void Main(String[] args)
     {
-        Console.WriteLine(String.Join(" ", SolutionMoveZeroes.MoveZeroes([0,1,0,0,3,4,0,5,0])));
+        Console.WriteLine(SolutionContainsDuplicate.ConstainsDuplicate([0,1,0,0,3,4,0,5,0]));
     }
 }
